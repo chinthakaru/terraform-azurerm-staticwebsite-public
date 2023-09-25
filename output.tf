6282f92127968dee52891ec35dba89815e83eeb0
Chinthaka Rupasinghe <chinthakaru@gmail.com>
2023-09-25 11:42:26 +0530

    :q

--- output.tf
+++ output.tf
@@ -2,22 +2,22 @@
 
 output "resource_group_id" {
   description = "resource group id"
-  value       = module.Azure_Static_WebSite.resource_group_id
+  value       = azurerm_resource_group.this_rg.id
 }
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = module.Azure_Static_WebSite.resource_group_name
+  value       = azurerm_resource_group.this_rg.name
 }
 output "resource_group_location" {
   description = "resource group location"
-  value       = module.Azure_Static_WebSite.resource_group_location
+  value       = azurerm_resource_group.this_rg.location
 }
 output "storage_account_id" {
   description = "storage account id"
-  value       = module.Azure_Static_WebSite.storage_account_id
+  value       = azurerm_storage_account.storage.id
 }
 output "storage_account_name" {
   description = "storage account name"
-  value       = module.Azure_Static_WebSite.storage_account_name
+  value       = azurerm_storage_account.storage.name
 }
 
